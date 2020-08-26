b655d308d544abac2f58772a329f77a2da6ba00e
Steven Stapleton <ssstevenstapleton@gmail.com>
2020-08-26 15:17:46 -0400

    Connection to omdb

---
+++ src/axios/movies.tsx
@@ -0,0 +1,10 @@
+import axios from './axios-movies';
+
+class Movies {
+    getMovies(query: String) {
+        console.log('Getting movies.');
+        return (<h1>Tst</h1>);
+    }
+}
+
+export default Movies;
