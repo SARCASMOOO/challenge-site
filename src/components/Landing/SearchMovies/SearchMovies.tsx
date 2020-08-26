39875a3ff0f46ad7babfae6409dccd756f6ae3fc
Steven Stapleton <ssstevenstapleton@gmail.com>
2020-08-26 13:54:58 -0400

    Created and styled movie search header.

--- src/components/Landing/SearchMovies/SearchMovies.tsx
+++ src/components/Landing/SearchMovies/SearchMovies.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import styles from './SearchMovies.module.css';
+import Container  from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 
-interface Props {}
+interface Props {
+}
 
 const SearchMovies = ({}: Props) => (
-    <h1>Movies Search</h1>
+    <Container>
+        <Typography variant='h3' component='h3' className={styles.Title} align='center'>Shopify Award Show</Typography>
+        <h1>Search</h1>
+        <h1>Results</h1>
+    </Container>
 );
 
 export default SearchMovies;
