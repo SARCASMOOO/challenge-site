d05d6fc08599b557576b534d98fe14bc114a2269
Steven Stapleton <ssstevenstapleton@gmail.com>
2020-08-26 12:53:53 -0400

    Crated navigation compoennt and styled background.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import {StylesProvider} from '@material-ui/core/styles';
 import Layout from "./Layout/Layout";
+import Navigation from "./Navigation/Navigation";
+
 import './App.css';
 
 function App() {
     return (
         <StylesProvider injectFirst>
             <Layout>
-                <nav>Navigation</nav>
+                <Navigation/>
                 <main>Main content</main>
                 <footer>Footer</footer>
             </Layout>
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,12 @@
+import React, {Fragment} from "react";
+import styles from './Navigation.module.css';
+
+interface Props {}
+
+const Navigation = ({}: Props) => (
+    <div className={styles.Navigation}>
+        <h1>Navigation</h1>
+    </div>
+);
+
+export default Navigation;
