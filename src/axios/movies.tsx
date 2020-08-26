144c06effc9c5699aed58301e092fac74a82f0bd
Steven Stapleton <ssstevenstapleton@gmail.com>
2020-08-26 15:36:23 -0400

    Stored OMDB key in development env variable.

--- src/axios/movies.tsx
+++ src/axios/movies.tsx
@@ -1,11 +1,12 @@
 import axios from './axios-movies';
 import React from "react";
+require('dotenv').config();
 
 class Movies {
-    private imdbURL = 'http://www.omdbapi.com/?i=tt3896198';
+    private imdbURL = 'http://www.omdbapi.com/?i=tt3896198&apikey=' + process.env['REACT_APP_OM_DB_API_KEY'];
 
     getMovies(query: String) {
-        console.log('test');
+        console.log(this.imdbURL);
         return (<h1>s</h1>);
     }
 }
