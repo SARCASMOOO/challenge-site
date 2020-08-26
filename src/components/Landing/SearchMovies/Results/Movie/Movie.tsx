7274708f06729366badefd3004d8d865dff6e00c
Steven Stapleton <ssstevenstapleton@gmail.com>
2020-08-26 17:39:21 -0400

    Made card not clickable.

--- src/components/Landing/SearchMovies/Results/Movie/Movie.tsx
+++ src/components/Landing/SearchMovies/Results/Movie/Movie.tsx
@@ -5,22 +5,20 @@ import styles from './Movie.module.css';
 
 const Movie = ({Poster, Title, Year, imdbID}: MovieModel) => (console.log(Poster, Title, Year, imdbID),
     <Card className={styles.Movie}>
-        <CardActionArea>
-            <CardMedia
-                style={{height: '250px'}}
-                component='img'
-                src={Poster}
-                title={Title}
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                    {Year}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                    {imdbID}
-                </Typography>
-            </CardContent>
-        </CardActionArea>
+        <CardMedia
+            style={{height: '250px'}}
+            component='img'
+            src={Poster}
+            title={Title}
+        />
+        <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+                {Year}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+                {imdbID}
+            </Typography>
+        </CardContent>
         <CardActions>
             <Button size="small" color="primary">
                 Nominate
