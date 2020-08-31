18198b819326705a600041172e2cb532fe27f719
Steven Stapleton <ssstevenstapleton@gmail.com>
2020-08-31 17:27:14 -0400

    Cleaning up unused imports.

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from "react";
+import React from "react";
 import styles from './Layout.module.css';
 
 function Layout({children}: React.PropsWithChildren<{}>) {   
